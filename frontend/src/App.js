29e79ca085b5ac060b968c929d0a001ef596a5b3
Jenushan <jenushan349@gmail.com>
2025-06-22 22:05:11 -0400

    Render fetched alerts to UI using .map()

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,17 +4,24 @@ import React, { useEffect, useState } from 'react';
 
 
 function App() {
+
+  const [alerts, setAlerts] = useState([]) // Holds array of alert data
+
   useEffect(() => {
-    fetch("http://localhost:5000/subway_alerts")
-      .then(res => res.json())
+    fetch("http://localhost:5000/subway_alerts") // Get data from Flask 
+      .then(res => res.json()) // Turn the response into JSON
       .then(data => {
         console.log("Fetched delay:", data);
+        setAlerts(data) // Save it into a state so React can use it
       });
-  }, []);
+  }, []); // Only runs one time, when the page first loads
 
   return (
     <div className="App">
-
+      {alerts.map(alert => (
+        alert.title,
+        alert.description
+      ))}
     </div>
   );
 }
