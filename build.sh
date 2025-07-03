d076e1100195e8375b28defda61d1fd913e78fb8
Jenushan <jenushan349@gmail.com>
2025-07-03 14:09:41 -0400

    Reorder build.sh to deploy

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+python3 -m grpc_tools.protoc -I=backend --python_out=backend backend/gtfs-realtime.proto
+
 cd frontend
 npm ci
 npm run build
 cd ..
 
-python3 -m grpc_tools.protoc -I=backend --python_out=backend backend/gtfs-realtime.proto
